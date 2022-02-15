ea4bbaf9db67c99f63abb1063f470f93974bbfa4
Rostuslav Bilovus <rosbilovus@gmail.com>
2022-02-15 22:45:15 +0200

    travis

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/steam-angular'));
 
-app.get('/*', (req, res) =>
+app.get('/', (req, res) =>
     res.sendFile('index.html', {root: 'dist/steam-angular/'}),
 );
 
