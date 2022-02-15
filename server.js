8f063b4c8e488b99d13b7440928a9a7a322a2d66
Rostuslav Bilovus <rosbilovus@gmail.com>
2022-02-16 01:21:03 +0200

    internal server error fix p-1

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 8080
+const express = require('express');
+const path = require('path');
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .get('/', (req, res) => res.render('dist/steam-angular/index.html'))
-    .listen(PORT, () => console.log(`Listening on ${PORT}`))
+const app = express();
+
+
+app.use(express.static(__dirname + '/dist/steam-angular'));
+
+app.get('/*', function(req,res) {
+ 
+res.sendFile(path.join(__dirname+'/dist/steam-angular/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
+// const express = require('express')
+// const path = require('path')
+// const PORT = process.env.PORT || 8080
+
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .get('/', (req, res) => res.render('dist/steam-angular/index'))
+//     .listen(PORT, () => console.log(`Listening on ${PORT}`))
   
 // //Install express server
 // const express = require('express');
