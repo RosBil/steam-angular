91eab00dc1723ff7085a676be460102fad879c41
Serhii Staryk <serhii.stary@gmail.com>
2022-02-17 13:46:08 +0200

    added FormsModule

--- src/app/friends/friends.module.ts
+++ src/app/friends/friends.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FriendsComponent } from './friends.component';
 import { FriendItemComponent } from './friend-item/friend-item.component';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -11,7 +12,8 @@ import { FriendItemComponent } from './friend-item/friend-item.component';
     FriendItemComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ]
 })
 export class FriendsModule { }
