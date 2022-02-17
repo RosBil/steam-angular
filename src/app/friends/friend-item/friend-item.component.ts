e3d069dfcff4a71c1e92e1deae772e14804d1446
Serhii Staryk <serhii.stary@gmail.com>
2022-02-17 13:47:41 +0200

    added basic functionality

--- src/app/friends/friend-item/friend-item.component.ts
+++ src/app/friends/friend-item/friend-item.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Friend } from 'src/app/shared/interfaces/friend.interface';
+import { FriendsServices } from 'src/app/shared/services/friend.service';
 
 @Component({
   selector: 'app-friend-item',
   templateUrl: './friend-item.component.html',
-  styleUrls: ['./friend-item.component.css']
+  styleUrls: ['./friend-item.component.scss'],
 })
 export class FriendItemComponent implements OnInit {
-  
-  constructor() { }
+  @Input() friend!: Friend;
 
-  ngOnInit(): void {
+  constructor(private friendService: FriendsServices) {}
+
+  removeFriend(friend: Friend): void {
+    this.friendService
+      .setMyFriend({ ...friend, isFriend: false })
+      .subscribe(() => {
+        console.log(`You remove ${friend.nickname} from friends`);
+      });
   }
 
+  addFriend(friend: Friend): void {
+    this.friendService
+      .setMyFriend({ ...friend, isFriend: true })
+      .subscribe(() => {
+        console.log(`You add ${friend.nickname} from friends`);
+      });
+  }
+
+  ngOnInit(): void {}
 }
