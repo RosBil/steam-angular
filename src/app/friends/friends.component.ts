dbafa8a1d97741ad1eb076720c55b06853cef7ac
Serhii Staryk <serhii.stary@gmail.com>
2022-02-17 13:47:31 +0200

    added basic functionality

--- src/app/friends/friends.component.ts
+++ src/app/friends/friends.component.ts
@@ -1,35 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Friend } from '../shared/interfaces/friend.interface';
 import { FriendsServices } from '../shared/services/friend.service';
 
 @Component({
   selector: 'app-friends',
   templateUrl: './friends.component.html',
-  styleUrls: ['./friends.component.css']
+  styleUrls: ['./friends.component.scss'],
 })
 export class FriendsComponent implements OnInit {
-  isSearching = true;
-  friendsArr: Friend[] = [];
-  constructor(private friendsService: FriendsServices) { }
-  deleteFriend(deletedFriend: Friend): void {
-    this.friendsArr.filter((friend) => friend.nickname !== deletedFriend.nickname);
-  }
-  addFriend(person: Friend): void {
-    this.friendsService.setMyFriend(person).subscribe(() => console.log(`friend is added`));
-    /* this function works in both ways */
+  friendsList$!: Observable<Friend[]>;
 
-    /* this.friendsService.getMyFriends().subscribe((arrOfFriends)=> this.friendsArr = arrOfFriends); */
+  friendsSearchList: Friend[] = [];
+
+  searchName = '';
+
+  constructor(private friendsService: FriendsServices) {}
+
+  searchFriend(name: string): void {
+    this.friendsService.getFriendsByName(name).subscribe((friends) => {
+      this.friendsSearchList = friends;
+    });
   }
-  onSearch(name: string): void {
-    this.friendsService.getFriendsByName(name).subscribe((arr) => this.friendsArr = arr);
-    console.log('searching friend');
-  }
-  onStopSearching(): void {
-    this.isSearching = false;
-    console.log('end search');
-  }
+
   ngOnInit(): void {
+    this.friendsList$ = this.friendsService.getMyFriends();
   }
-
-
 }
