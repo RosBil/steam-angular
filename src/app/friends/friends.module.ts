de71e562333739842cb7eab022bf86808ed3455c
Serhii Staryk <serhii.stary@gmail.com>
2022-02-18 15:06:39 +0200

    added FriendsServices to providers

--- src/app/friends/friends.module.ts
+++ src/app/friends/friends.module.ts
@@ -3,17 +3,13 @@ import { CommonModule } from '@angular/common';
 import { FriendsComponent } from './friends.component';
 import { FriendItemComponent } from './friend-item/friend-item.component';
 import { FormsModule } from '@angular/forms';
-
-
+import { SharedModule } from '../shared/shared.module';
+import { FriendsServices } from '../shared/services/friend.service';
 
 @NgModule({
-  declarations: [
-    FriendsComponent,
-    FriendItemComponent
-  ],
-  imports: [
-    CommonModule,
-    FormsModule
-  ]
+  declarations: [FriendsComponent, FriendItemComponent],
+  imports: [CommonModule, FormsModule, SharedModule],
+  providers: [FriendsServices],
+
 })
-export class FriendsModule { }
+export class FriendsModule {}
