d336a057d350c447b73a0320cfe5fa076611c7df
Serhii Staryk <serhii.stary@gmail.com>
2022-02-19 20:53:21 +0200

    added sharedModule

--- src/app/library/library.module.ts
+++ src/app/library/library.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LibraryComponent } from './library.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -9,7 +10,8 @@ import { LibraryComponent } from './library.component';
     LibraryComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ]
 })
 export class LibraryModule { }
