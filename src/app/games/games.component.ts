0d6f109a6c93ef1a706e8aa4394c75666624baff
Roman_Sikanda <roman_sikanda@epam.com>
2022-02-21 17:44:06 +0200

    deleted comments

--- src/app/games/games.component.ts
+++ src/app/games/games.component.ts
@@ -2,9 +2,8 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { Game } from '../shared/interfaces/game.interface';
-import { AuthService } from '../shared/services/auth.service';
-/* import { GamesServices } from '../shared/services/game.service'; */
-import { SearchService } from '../shared/services/search.service';
+import { AuthService } from '../core/services/auth.service';
+import { SearchService } from '../core/services/search.service';
 
 @Component({
   selector: 'app-games',
@@ -21,32 +20,17 @@ export class GamesComponent implements OnInit, OnDestroy {
   errorSubscription: Subscription = new Subscription();
 
   constructor(
-    /* private gamesService: GamesServices, */
     private searchService: SearchService,
     private auth: AuthService,
     private route: ActivatedRoute,
     private router: Router
   ) { }
 
-  /* getGames(): void {
-    this.gamesList = this.searchService.gameList.slice();
-  } */
-
   searchGame(name: string): void {
     this.searchService.setSearchPhrase(name);
     this.searchService.search();
   }
 
-  /* searchGameByPrice(price: number): void {
-    this.gamesService
-      .getGamesByPrice(price)
-      .subscribe((data) => (this.gamesList = data));
-  } */
-  /* searchByTag(tagArr: string[]): void {
-    this.gamesService
-      .getGamesByTag(tagArr)
-      .subscribe((data) => (this.gamesList = data));
-  } */
   showAlert(message: string) {
     if (message?.length) {
       alert(message);
